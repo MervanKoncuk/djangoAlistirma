6fedb0870a189e209845cd98b70fcab93bf9d0f8
Mervan Koncuk <mervankoncuk@gmail.com>
2023-07-07 11:35:09 +0300

    add ignore and test

--- ogrenciler/tests.py
+++ ogrenciler/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Ogrenci
+
+class OgrenciTest(TestCase):
+    def test_ogrenci_olusturma(self):
+        ogrenci = Ogrenci.objects.create(isim='Ahmet', aciklama='Öğrenci 1', numara='12345')
+        # Oluşturulan öğrencinin bilgileri ve alanları uyuşuyor mu diye kontrol ediliyor
+        self.assertEqual(ogrenci.isim, 'Ahmet')
+        self.assertEqual(ogrenci.aciklama, 'Öğrenci 1')
+        self.assertEqual(ogrenci.numara, '12345')
